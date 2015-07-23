3ced924e5292cdca0bf16ea50829b1995c61557f
Sebastian Kohler <baestu.kohler@gmail.com>
2015-07-23 15:52:21 +0200

    improve unit test

--- src/test/scala/RecommendationRequestSpec.scala
+++ src/test/scala/RecommendationRequestSpec.scala
@@ -8,9 +8,9 @@ class RecommendationRequestSpec extends FlatSpec with MockFactory with Matchers 
     val jsonString = "{\"itemScores\":[{\"item\":\"52\",\"score\":9.582509402541834},{\"item\":\"95\",\"score\":8.017236650368387}]}"
     val engineResponse = new JsonParser().parse(jsonString).asInstanceOf[JsonObject]
 
-    val engine = stub[RecommendationEngine]
-    (engine.sendQuery _).when(*).returning(engineResponse)
+    val engine = mock[RecommendationEngine]
+    (engine.sendQuery _).expects(*).once().returning(engineResponse)
 
-    new RecommendationRequest(engine).getRecommendations("userId") should be (List("52", "95"))
+    new RecommendationRequest(engine).getRecommendations("userId") should contain theSameElementsAs List("52", "95")
   }
 }
