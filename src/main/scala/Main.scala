b0606a2bce82e5b203672b43a75db7040e2a7551
Sebastian Kohler <baestu.kohler@gmail.com>
2015-07-23 09:45:51 +0200

    add user loop

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,3 +1,15 @@
 object Main {
-    def main(args: Array[String]) = println("Hello World!")
+    def main(args: Array[String]) = {
+        val users : List[String] = List("a.v.hermann@gmail.com", "baestu.kohler@gmail.com")
+
+        users.foreach(processUser)
+    }
+
+    def processUser(user : String) = {
+        sendMail(user, "Giusi's")
+    }
+
+    def sendMail(user: String, recommendation : String) = {
+        println("Recommendations for " + user + ": " + recommendation)
+    }
 }
