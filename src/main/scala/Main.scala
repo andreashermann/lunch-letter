df8e182932a555a9323193f59aeb22ca8deb2e78
Sebastian Kohler <baestu.kohler@gmail.com>
2015-07-23 19:17:24 +0200

    lookup restaurants for each user

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,16 +1,21 @@
+import com.google.gson.JsonElement
 import main.scala.SmtpMailer
-import scala.collection.JavaConversions._
+import scala.collection.mutable.Map
 
 object Main {
   def main(args: Array[String]) = {
     val recommendationRequest = new RecommendationRequest(new RecommendationEngine)
+    val restaurantsById = recommendationRequest.getRestaurantsById();
     val users = recommendationRequest.getUserIds()
     val mailer = new SmtpMailer
-    users.foreach(x => processUser(x, recommendationRequest, mailer))
+    users.foreach(x => processUser(x, restaurantsById, recommendationRequest, mailer))
   }
 
-  def processUser(emailAddress: String, recommendationRequest : RecommendationRequest, mailer : SmtpMailer) = {
+  def processUser(emailAddress: String, restaurantsById: Map[String, Map[String, JsonElement]],
+                  recommendationRequest: RecommendationRequest, mailer: SmtpMailer) = {
     val recommendationIds = recommendationRequest.getRecommendationIds(emailAddress)
+    val restraurants = recommendationIds.map(id => restaurantsById.get(id))
+
     val body = "Giusi's"
     mailer.send(emailAddress, "LunchLetter Recommendations Do 23.7.", body)
     //sendMail(user, "Giusi's")
