b9f6a98a63ab1b052e404c6d1d7b632256908089
Sebastian Kohler <baestu.kohler@gmail.com>
2015-07-23 15:01:14 +0200

    fix AnyRef compilation issue

--- src/main/scala/RecommendationRequest.scala
+++ src/main/scala/RecommendationRequest.scala
@@ -6,7 +6,7 @@ import collection.JavaConversions._
 class RecommendationRequest(engineClient: EngineClient) {
 
   def getRecommendations(userId: String) : List[String] = {
-    val arguments : java.util.Map[String,AnyRef] = HashMap[String,AnyRef]("userEntityId" -> userId, "number" -> 3)
+    val arguments : java.util.Map[String,AnyRef] = HashMap("userEntityId" -> userId.asInstanceOf[AnyRef], "number" -> 3.asInstanceOf[AnyRef])
     val response = engineClient.sendQuery(arguments)
 
     response.getAsJsonArray("itemScores")
