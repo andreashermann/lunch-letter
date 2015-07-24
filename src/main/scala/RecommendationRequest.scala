3bf59b3a0fd12627a3046ebfe44de4d9f0c9b1ce
Sebastian Kohler <baestu.kohler@gmail.com>
2015-07-24 20:17:56 +0200

    add some more random pictures

--- src/main/scala/RecommendationRequest.scala
+++ src/main/scala/RecommendationRequest.scala
@@ -28,7 +28,10 @@ class RecommendationRequest(engineClient: RecommendationEngine) {
       "https://farm8.staticflickr.com/7476/15821829991_68ac4aa8e6_n.jpg",
       "https://farm8.staticflickr.com/7553/15821827311_201e5419dd_n.jpg",
       "https://farm4.staticflickr.com/3667/9356160984_fe56ab0015_n.jpg",
-      "https://farm3.staticflickr.com/2877/9006446155_9eca1163ae_n.jpg"
+      "https://farm3.staticflickr.com/2877/9006446155_9eca1163ae_n.jpg",
+      "https://farm3.staticflickr.com/2101/2529856456_368530c5ab_n.jpg",
+      "https://farm1.staticflickr.com/37/80200323_2efeb7601c.jpg",
+      "https://farm6.staticflickr.com/5484/14330259870_eb11fe531d_n.jpg"
     ))).flatten
 
     engineClient.getRestaurants().map(_.getAsJsonObject)
