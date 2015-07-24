816c3f3448327c9a4b82ee02b531a61d5fcbd6ed
Andreas Hermann <a.v.hermann@gmail.com>
2015-07-24 09:17:55 +0200

    configurable ENGINE_ACCESS_KEY

--- src/main/scala/RecommendationEngine.scala
+++ src/main/scala/RecommendationEngine.scala
@@ -6,8 +6,9 @@ import scala.io.Source
 
 class RecommendationEngine {
 
-  //val host = "46.101.237.209:7070"
-  val host = "engine:80"
+  val host = "www.lunchletter.ch:7070"
+
+  val ENGINE_ACCESS_KEY = System.getenv("ENGINE_ACCESS_KEY")
 
   def getRecommendations(arguments: Map[String, AnyRef]): JsonObject = {
     val engine = new EngineClient("http://" + host)
@@ -15,12 +16,12 @@ class RecommendationEngine {
   }
 
   def getUsers() : JsonArray = {
-    val url = "http://" + host + "/events.json?event=add_user&accessKey=swBRCzjYKNlmEHdAHBN6WEg9vVcxkt5NzbWc96w2E9xBElfWV1SIXhO6MPZA4CSf"
+    val url = "http://" + host + "/events.json?event=add_user&accessKey=" + ENGINE_ACCESS_KEY
     new JsonParser().parse(Source.fromURL(url).mkString).asInstanceOf[JsonArray]
   }
 
   def getRestaurants() : JsonArray = {
-    val url = "http://" + host + "/events.json?event=add_restaurant&accessKey=swBRCzjYKNlmEHdAHBN6WEg9vVcxkt5NzbWc96w2E9xBElfWV1SIXhO6MP"
+    val url = "http://" + host + "/events.json?event=add_restaurant&accessKey=" + ENGINE_ACCESS_KEY
     new JsonParser().parse(Source.fromURL(url).mkString).asInstanceOf[JsonArray]
   }
 }
