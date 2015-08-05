d8a8ebf37f4355a4325367116d70041cebfd335c
Andreas Hermann <a.v.hermann@gmail.com>
2015-08-06 00:01:02 +0200

    add email view event tracker image

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -7,13 +7,14 @@ import scala.collection.immutable.Map
 object Main {
 
   val engine = new RecommendationEngine() // new RecommendationEngine
-  val mailer = new SmtpMailer // new SmtpMailer
+  val mailer = new StubMailer // new SmtpMailer
 
-  def main(args: Array[String]) = {
+  def main(args: Array[String]) : Unit = {
     val recommendationRequest = new RecommendationRequest(engine)
     val restaurantsById = recommendationRequest.getRestaurantsById();
     val users = recommendationRequest.getUserIds()
     users.foreach(x => processUser(x, restaurantsById, recommendationRequest, mailer))
+    sys.exit(0)
   }
 
   def processUser(emailAddress: String, restaurantsById: Map[String, Map[String, String]],
@@ -25,5 +26,7 @@ object Main {
     val date = LocalDate.now().toString
     val body = new TemplateRenderer().render(emailAddress, restaurants)
     mailer.send(emailAddress, "LunchLetter Recommendations " + date, body)
+    
+    // TODO: save last sent date
   }
 }
